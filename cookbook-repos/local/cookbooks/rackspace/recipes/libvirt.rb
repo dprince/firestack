b55ef3701da67c1c3571bc5a7e6a261a36bebf6d
Dan Prince <dan.prince@rackspace.com>
2011-03-03 22:38:39 -0500

    Install libvirt0.

--- cookbook-repos/local/cookbooks/rackspace/recipes/libvirt.rb
+++ cookbook-repos/local/cookbooks/rackspace/recipes/libvirt.rb
@@ -1,3 +1,9 @@
+package "libvirt0" do
+  options "--force-yes"
+  version node[:libvirt][:version]
+  action :install
+end
+
 package "libvirt-bin" do
   options "--force-yes"
   version node[:libvirt][:version]
