79487867af2b1a8256df954fd4e21dd8584de20e
Dan Prince <dprince@redhat.com>
2012-11-05 09:46:11 -0500

    Update manifests with latest upstream changes.

--- config/puppet-configs/single_node_mysql/nova1.pp
+++ config/puppet-configs/single_node_mysql/nova1.pp
@@ -11,9 +11,8 @@ $glance_api_servers = 'localhost:9292'
 $glance_host        = 'localhost'
 $glance_port        = '9292'
 
-$api_server = 'localhost'
-
 $nova_network = '192.168.0.0/24'
+$available_ips = '256'
 $floating_network = '172.20.0.0/24'
 
 $lock_path = '/var/lib/nova/tmp'
@@ -33,6 +32,19 @@ $keystone_db_name     = 'keystone'
 $keystone_db_user = 'keystone'
 $keystone_db_password = 'password'
 $keystone_sql_connection = "mysql://${keystone_db_user}:${keystone_db_password}@${keystone_db_host}/${keystone_db_name}"
+
+$cinder_db_driver     = 'mysql'
+$cinder_db_host     = 'localhost'
+$cinder_db_name     = 'cinder'
+$cinder_db_user = 'cinder'
+$cinder_db_password = 'password'
+
+$cinder_lock_path = '/var/lib/cinder/tmp'
+
+$cinder_qpid_password = 'p@ssw0rd'
+$cinder_qpid_user = 'cinder_qpid'
+$cinder_qpid_realm = 'OPENSTACK'
+
 
 resources { 'nova_config':
   purge => true,
@@ -74,7 +86,6 @@ class { 'keystone::api':
   require => [Class["keystone::mysql"], Class["mysql::server"]]
 }
 
-
 class { 'nova::mysql':
   password      => $db_password,
   dbname        => $db_name,
@@ -82,13 +93,11 @@ class { 'nova::mysql':
   host          => $db_host,
 }
 
-class { 'nova::controller':
-  db_driver => $db_driver,
-  db_password => $db_password,
-  db_name => $db_name,
-  db_user => $db_user,
-  db_host => $db_host,
 
+class { 'cinder::client': }
+
+class { 'nova':
+  sql_connection => "${db_driver}://${db_user}:${db_password}@${db_host}/${db_name}",
   image_service => 'nova.image.glance.GlanceImageService',
 
   glance_api_servers => $glance_api_servers,
@@ -97,24 +106,52 @@ class { 'nova::controller':
 
   libvirt_type => 'qemu',
 
-  nova_network => $nova_network,
-  floating_network => $floating_network,
   force_dhcp_release => true,
-  keystone_enabled => true,
   scheduler_default_filters => 'AvailabilityZoneFilter,ComputeFilter',
   allow_resize_to_same_host => true,
   libvirt_wait_soft_reboot_seconds => 15,
   rpc_backend => 'nova.rpc.impl_qpid',
   qpid_username => $qpid_user,
   qpid_password => $qpid_password,
-  require => [Class["keystone"], Class["nova::mysql"], Class["mysql::server"]]
+  enabled_apis => 'ec2,osapi_compute,metadata',
+  volume_api_class => 'nova.volume.cinder.API',
+  require => [Class["keystone"], Class["nova::mysql"], Class["mysql::server"], Class["cinder::client"]]
 }
 
+  class { "nova::api": enabled => true, keystone_enabled => true }
+
+  $flat_network_bridge  = 'br100'
+  $flat_network_bridge_ip  = '11.0.0.1'
+  $flat_network_bridge_netmask  = '255.255.255.0'
+  class { "nova::network::flat":
+    enabled                     => true,
+    flat_network_bridge         => $flat_network_bridge,
+    flat_network_bridge_ip      => $flat_network_bridge_ip,
+    flat_network_bridge_netmask => $flat_network_bridge_netmask,
+  }
+
+  class { "nova::objectstore":
+    enabled => true,
+  }
+
+  class { "nova::cert":
+    enabled => true,
+  }
+
+
+  class { "nova::scheduler": enabled => true }
+
+  nova::manage::network { "net-${nova_network}":
+    network       => $nova_network,
+    available_ips => $available_ips
+  }
+
+  nova::manage::floating { "floating-${floating_network}":
+    network       => $floating_network
+  }
+
 class { 'nova::compute':
-  api_server     => $api_server,
-  enabled        => true,
-  api_port       => 8773,
-  aws_address    => '169.254.169.254',
+  enabled        => true
 }
 
 class { 'glance::mysql':
@@ -135,3 +172,38 @@ class { 'glance::api':
   sql_connection => $glance_sql_connection,
   require => [Class["keystone"], Class["glance::mysql"], Class["mysql::server"], Class["glance::registry"]]
 }
+
+resources { 'cinder_config':
+  purge => true,
+}
+class { 'cinder::qpid':
+  user => $cinder_qpid_user,
+  password => $cinder_qpid_password,
+  realm => $cinder_qpid_realm,
+}
+
+
+class { 'cinder::mysql':
+  password      => $cinder_db_password,
+  dbname        => $cinder_db_name,
+  user          => $cinder_db_username,
+  host          => $cinder_db_host,
+}
+
+class { 'cinder':
+  db_driver => $cinder_db_driver,
+  db_password => $cinder_db_password,
+  db_name => $cinder_db_name,
+  db_user => $cinder_db_user,
+  db_host => $cinder_db_host,
+  rpc_backend => 'cinder.openstack.common.rpc.impl_qpid',
+  qpid_username => $cinder_qpid_user,
+  qpid_password => $cinder_qpid_password,
+  auth_strategy => 'keystone',
+  scheduler_driver => 'cinder.scheduler.chance.ChanceScheduler',
+  require => [Class["cinder::mysql"], Class["mysql::server"]]
+}
+
+class { 'cinder::api': }
+class { 'cinder::scheduler': }
+class { 'cinder::volume': }
--- config/puppet-configs/single_node_postgres/nova1.pp
+++ config/puppet-configs/single_node_postgres/nova1.pp
@@ -1,4 +1,4 @@
-$db_driver     = 'postgresql'
+ver     = 'postgresql'
 $db_host     = 'localhost'
 $db_name     = 'nova'
 $db_user = 'nova'
@@ -11,9 +11,8 @@ $glance_api_servers = 'localhost:9292'
 $glance_host        = 'localhost'
 $glance_port        = '9292'
 
-$api_server = 'localhost'
-
 $nova_network = '192.168.0.0/24'
+$available_ips = '256'
 $floating_network = '172.20.0.0/24'
 
 $lock_path = '/var/lib/nova/tmp'
@@ -34,10 +33,22 @@ $keystone_db_user = 'keystone'
 $keystone_db_password = 'password'
 $keystone_sql_connection = "postgresql://${keystone_db_user}:${keystone_db_password}@${keystone_db_host}/${keystone_db_name}"
 
+$cinder_db_driver     = 'postgresql'
+$cinder_db_host     = 'localhost'
+$cinder_db_name     = 'cinder'
+$cinder_db_user = 'cinder'
+$cinder_db_password = 'password'
+
+$cinder_lock_path = '/var/lib/cinder/tmp'
+
+$cinder_qpid_password = 'p@ssw0rd'
+$cinder_qpid_user = 'cinder_qpid'
+$cinder_qpid_realm = 'OPENSTACK'
+
+
 resources { 'nova_config':
   purge => true,
 }
-
 class { 'qpid::server':
   realm => $qpid_realm,
 }
@@ -54,6 +65,7 @@ class { 'postgresql::python': }
 
 class { 'keystone': }
 
+
 class { 'keystone::postgresql':
   db_password      => $keystone_db_password,
   db_name        => $keystone_db_name,
@@ -66,6 +78,7 @@ class { 'keystone::api':
   require => [Class["keystone::postgresql"], Class["postgresql::python"]]
 }
 
+
 class { 'nova::postgresql':
   db_password      => $db_password,
   db_name        => $db_name,
@@ -74,13 +87,11 @@ class { 'nova::postgresql':
   require => Class["postgresql::python"]
 }
 
-class { 'nova::controller':
-  db_driver => $db_driver,
-  db_password => $db_password,
-  db_name => $db_name,
-  db_user => $db_user,
-  db_host => $db_host,
+class { 'cinder::client': }
 
+
+class { 'nova':
+  sql_connection => "${db_driver}://${db_user}:${db_password}@${db_host}/${db_name}",
   image_service => 'nova.image.glance.GlanceImageService',
 
   glance_api_servers => $glance_api_servers,
@@ -89,87 +100,52 @@ class { 'nova::controller':
 
   libvirt_type => 'qemu',
 
-  nova_network => $nova_network,
-  floating_network => $floating_network,
-  keystone_enabled => true,
+  force_dhcp_release => true,
   scheduler_default_filters => 'AvailabilityZoneFilter,ComputeFilter',
   allow_resize_to_same_host => true,
   libvirt_wait_soft_reboot_seconds => 15,
   rpc_backend => 'nova.rpc.impl_qpid',
   qpid_username => $qpid_user,
   qpid_password => $qpid_password,
-  require => [Class["keystone"], Class["nova::postgresql"], Class["postgresql::python"]]
+  enabled_apis => 'ec2,osapi_compute,metadata',
+  volume_api_class => 'nova.volume.cinder.API',
+  require => [Class["keystone"], Class["nova::postgresql"], Class["postgresql::server"], Class["cinder::client"]]
 }
+
+  class { "nova::api": enabled => true, keystone_enabled => true }
+
+  $flat_network_bridge  = 'br100'
+  $flat_network_bridge_ip  = '11.0.0.1'
+  $flat_network_bridge_netmask  = '255.255.255.0'
+  class { "nova::network::flat":
+    enabled                     => true,
+    flat_network_bridge         => $flat_network_bridge,
+    flat_network_bridge_ip      => $flat_network_bridge_ip,
+    flat_network_bridge_netmask => $flat_network_bridge_netmask,
+  }
+
+  class { "nova::objectstore":
+    enabled => true,
+  }
+
+  class { "nova::cert":
+    enabled => true,
+  }
+
+  class { "nova::scheduler": enabled => true }
+
+
+  nova::manage::network { "net-${nova_network}":
+    network       => $nova_network,
+    available_ips => $available_ips
+  }
+
+  nova::manage::floating { "floating-${floating_network}":
+    network       => $floating_network
+  }
 
 class { 'nova::compute':
-  api_server     => $api_server,
-  enabled        => true,
-  api_port       => 8773,
-  aws_address    => '169.254.169.254',
-}
-
-
-# Swift All In One
-$proxy_local_net_ip='127.0.0.1'
-$swift_shared_secret='PfppwiB1WkoodcnJjFkHrbm5OY'
-
-Exec { logoutput => true }
-
-class { 'ssh::server::install': }
-
-class { 'memcached':
-  listen_ip => $proxy_local_net_ip,
-}
-
-class { 'swift':
-  # not sure how I want to deal with this shared secret
-  swift_hash_suffix => $swift_shared_secret,
-  package_ensure => latest,
-}
-
-class { 'swift::storage': }
-
-# create xfs partitions on a loopback device and mounts them
-swift::storage::loopback { ['1', '2', '3']:
-  require => Class['swift'],
-  seek => '250000',
-}
-# sets up storage nodes which is composed of a single
-# device that contains an endpoint for an object, account, and container
-
-Swift::Storage::Node {
-  mnt_base_dir         => '/srv/node',
-  weight               => 1,
-  manage_ring          => true,
-  storage_local_net_ip => '127.0.0.1',
-}
-
-swift::storage::node { '1':
-  zone                 => 1,
-  require              => [Class['swift::storage'], Swift::Storage::Loopback[1]],
-}
-
-swift::storage::node { '2':
-  zone                 => 2,
-  require              => [Class['swift::storage'], Swift::Storage::Loopback[2]],
-}
-
-swift::storage::node { '3':
-  zone                 => 3,
-  require              => [Class['swift::storage'], Swift::Storage::Loopback[3]],
-}
-
-class { 'swift::ringbuilder':
-  part_power     => '18',
-  replicas       => '3',
-  min_part_hours => 1,
-  require        => Class['swift'],
-}
-
-class { 'swift::proxy':
-  auth_type => 'keystone',
-  account_autocreate => true,
-  require            => Class['swift::ringbuilder'],
+  enabled        => true
 }
 
 class { 'glance::postgresql':
@@ -188,11 +164,41 @@ class { 'glance::registry':
 class { 'glance::api':
   api_flavor => 'keystone+cachemanagement',
   sql_connection => $glance_sql_connection,
-  default_store => 'swift',
-  swift_store_auth_version => '2',
-  swift_store_auth_address => 'http://127.0.0.1:5000/v2.0/',
-  swift_store_user => 'admin:admin',
-  swift_store_key => 'AABBCC112233',
-  swift_store_create_container_on_put => 'True',
-  require => [Class["keystone"], Class["swift::proxy"], Class["glance::postgresql"], Class["postgresql::python"], Class["glance::registry"]]
+  require => [Class["keystone"], Class["glance::postgresql"], Class["postgresql::python"], Class["glance::registry"]]
 }
+
+resources { 'cinder_config':
+  purge => true,
+}
+
+class { 'cinder::qpid':
+  user => $cinder_qpid_user,
+  password => $cinder_qpid_password,
+  realm => $cinder_qpid_realm,
+}
+
+class { 'cinder::postgresql':
+  db_password      => $cinder_db_password,
+  db_name        => $cinder_db_name,
+  db_user          => $cinder_db_username,
+  db_host          => $cinder_db_host,
+  require => Class["postgresql::python"],
+}
+
+class { 'cinder':
+  db_driver => $cinder_db_driver,
+  db_password => $cinder_db_password,
+  db_name => $cinder_db_name,
+  db_user => $cinder_db_user,
+  db_host => $cinder_db_host,
+  rpc_backend => 'cinder.openstack.common.rpc.impl_qpid',
+  qpid_username => $cinder_qpid_user,
+  qpid_password => $cinder_qpid_password,
+  auth_strategy => 'keystone',
+  scheduler_driver => 'cinder.scheduler.chance.ChanceScheduler',
+  require => [Class["cinder::postgresql"], Class["postgresql::server"]]
+}
+
+class { 'cinder::api': }
+class { 'cinder::scheduler': }
+class { 'cinder::volume': }
