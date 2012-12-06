6a56b429e8207daf7d4da78c79eca0f2c7bff290
Dan Prince <dprince@redhat.com>
2012-12-06 12:47:34 -0500

    Add rc files for user1 and user2.
    Also sets OS_NO_CACHE to support latest python-keystoneclient
    changes.

--- scripts/keystone_data.sh
+++ scripts/keystone_data.sh
@@ -3,12 +3,15 @@
 export SERVICE_TOKEN=$SERVICE_TOKEN
 export SERVICE_ENDPOINT=$SERVICE_ENDPOINT
 export AUTH_ENDPOINT=$AUTH_ENDPOINT
+export OS_NO_CACHE=1
 
 function get_id () {
     echo `$@ | awk '/ id / { print $4 }'`
 }
 
 ADMIN_PASSWORD="AABBCC112233"
+USER1_PASSWORD="DDEEFF445566"
+USER2_PASSWORD="GGHHII778899"
 SERVICE_PASSWORD="SERVICE_PASSWORD"
 ADMIN_TENANT=`get_id keystone tenant-create --name=admin`
 SERVICE_TENANT=$(get_id keystone tenant-create --name=service)
@@ -23,11 +26,12 @@ ADMIN_USER=`get_id keystone user-create \
                                  --email=admin@example.com`
 USER1_USER=`get_id keystone user-create \
                                  --name=user1 \
-                                 --pass="DDEEFF445566" \
+                                 --pass="$USER1_PASSWORD" \
                                  --email=user1@example.com`
 USER2_USER=`get_id keystone user-create \
                                  --name=user2 \
                                  --pass="GGHHII778899" \
+                                 --pass="$USER2_PASSWORD" \
                                  --email=user2@example.com`
 
 # Roles
@@ -44,6 +48,8 @@ NETADMIN_ROLE=`get_id keystone role-create --name=netadmin`
 keystone user-role-add --user_id="$ADMIN_USER" \
                        --role_id="$ADMIN_ROLE" \
                        --tenant_id="$ADMIN_TENANT"
+
+#user1
 keystone user-role-add --user_id="$USER1_USER" \
                        --role_id="$MEMBER_ROLE" \
                        --tenant_id="$USER1_TENANT"
@@ -53,21 +59,31 @@ keystone user-role-add --user_id="$USER1_USER" \
 keystone user-role-add --user_id="$USER1_USER" \
                        --role_id="$NETADMIN_ROLE" \
                        --tenant_id="$USER1_TENANT"
-keystone user-role-add --user_id="$USER2_USER" \
-                       --role_id="$MEMBER_ROLE" \
-                       --tenant_id="$USER2_TENANT"
-keystone user-role-add --user_id="$USER2_USER" \
-                       --role_id="$SYSADMIN_ROLE" \
-                       --tenant_id="$USER2_TENANT"
-keystone user-role-add --user_id="$USER2_USER" \
-                       --role_id="$NETADMIN_ROLE" \
-                       --tenant_id="$USER2_TENANT"
 keystone user-role-add --user_id="$USER1_USER" \
                        --role_id="$MEMBER_ROLE" \
                        --tenant_id="$INVIS_TENANT"
 keystone user-role-add --user_id="$ADMIN_USER" \
                        --role_id="$ADMIN_ROLE" \
                        --tenant_id="$USER1_TENANT"
+
+#user2
+keystone user-role-add --user_id="$USER2_USER" \
+                       --role_id="$MEMBER_ROLE" \
+                       --tenant_id="$USER2_TENANT"
+keystone user-role-add --user_id="$USER2_USER" \
+                       --role_id="$SYSADMIN_ROLE" \
+                       --tenant_id="$USER2_TENANT"
+keystone user-role-add --user_id="$USER2_USER" \
+                       --role_id="$NETADMIN_ROLE" \
+                       --tenant_id="$USER2_TENANT"
+keystone user-role-add --user_id="$USER2_USER" \
+                       --role_id="$MEMBER_ROLE" \
+                       --tenant_id="$INVIS_TENANT"
+keystone user-role-add --user_id="$ADMIN_USER" \
+                       --role_id="$ADMIN_ROLE" \
+                       --tenant_id="$USER2_TENANT"
+
+#keystone admin
 keystone user-role-add --user_id="$ADMIN_USER" \
                        --role_id="$KEYSTONEADMIN_ROLE" \
                        --tenant_id="$ADMIN_TENANT"
@@ -167,14 +183,11 @@ RESULT=`keystone ec2-credentials-create --tenant_id=$USER2_TENANT --user_id=$USE
 USER2_ACCESS=`echo "$RESULT" | grep access | awk '{print $4}'`
 USER2_SECRET=`echo "$RESULT" | grep secret | awk '{print $4}'`
 
+cat > /root/.openstackrc <<EOF
+#COMMON ENV OPTIONS FOR FIRESTACK INIT SCRIPTS
 
-
-cat > /root/openstackrc <<EOF
-export OS_USERNAME=admin
-export OS_PASSWORD=$ADMIN_PASSWORD
-export OS_TENANT_NAME=admin
-export OS_AUTH_URL=$AUTH_ENDPOINT
-export OS_AUTH_STRATEGY=keystone
+# disable keyring caching in python-keystoneclient
+export OS_NO_CACHE=\${OS_NO_CACHE:-1}
 
 # legacy options for novaclient
 export NOVA_API_KEY="\$OS_PASSWORD"
@@ -182,8 +195,6 @@ export NOVA_USERNAME="\$OS_USERNAME"
 export NOVA_PROJECT_ID="\$OS_TENANT_NAME"
 export NOVA_URL="\$OS_AUTH_URL"
 export NOVA_VERSION="1.1"
-export EC2_ACCESS_KEY=$ADMIN_ACCESS
-export EC2_SECRET_KEY=$ADMIN_SECRET
 
 # Set the ec2 url so euca2ools works
 export EC2_URL=\$(keystone catalog --service ec2 | awk '/ publicURL / { print \$4 }')
@@ -197,4 +208,46 @@ export NOVA_CERT=\${NOVA_KEY_DIR}/cacert.pem
 export EUCALYPTUS_CERT=\${NOVA_CERT} # euca-bundle-image seems to require this set
 alias ec2-bundle-image="ec2-bundle-image --cert \${EC2_CERT} --privatekey \${EC2_PRIVATE_KEY} --user \${EC2_USER_ID} --ec2cert \${NOVA_CERT}"
 alias ec2-upload-bundle="ec2-upload-bundle -a \${EC2_ACCESS_KEY} -s \${EC2_SECRET_KEY} --url \${S3_URL} --ec2cert \${NOVA_CERT}"
+EOF
+
+#admin (openstackrc)
+cat > /root/openstackrc <<EOF
+export OS_USERNAME=admin
+export OS_PASSWORD=$ADMIN_PASSWORD
+export OS_TENANT_NAME=admin
+export OS_AUTH_URL=$AUTH_ENDPOINT
+export OS_AUTH_STRATEGY=keystone
+
+export EC2_ACCESS_KEY=$ADMIN_ACCESS
+export EC2_SECRET_KEY=$ADMIN_SECRET
+
+source .openstackrc
+EOF
+
+#user1
+cat > /root/user1rc <<EOF
+export OS_USERNAME=user1
+export OS_PASSWORD=$USER1_PASSWORD
+export OS_TENANT_NAME=user1
+export OS_AUTH_URL=$AUTH_ENDPOINT
+export OS_AUTH_STRATEGY=keystone
+
+export EC2_ACCESS_KEY=$USER1_ACCESS
+export EC2_SECRET_KEY=$USER1_SECRET
+
+source .openstackrc
+EOF
+
+#user2
+cat > /root/user2rc <<EOF
+export OS_USERNAME=user2
+export OS_PASSWORD=$USER2_PASSWORD
+export OS_TENANT_NAME=user2
+export OS_AUTH_URL=$AUTH_ENDPOINT
+export OS_AUTH_STRATEGY=keystone
+
+export EC2_ACCESS_KEY=$USER2_ACCESS
+export EC2_SECRET_KEY=$USER2_SECRET
+
+source .openstackrc
 EOF
