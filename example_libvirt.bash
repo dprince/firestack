f50feb8cea8c69bce338fb97b098a157c64257d3
Dan Prince <dprince@redhat.com>
2013-01-03 17:26:23 -0500

    Add custom torpedo test suite settings to example script.

--- example_libvirt.bash
+++ example_libvirt.bash
@@ -55,4 +55,11 @@ rake glance:load_images
 # Uncomment to configure quantum
 #
 #rake quantum:configure
+
+export TORPEDO_SERVER_BUILD_TIMEOUT=180
+export TORPEDO_SSH_TIMEOUT=120
+export TORPEDO_PING_TIMEOUT=60
+export TORPEDO_TEST_REBUILD_SERVER=true
+export TORPEDO_FLAVOR_REF=1
+export TORPEDO_IMAGE_NAME="ami-tty"
 rake torpedo
