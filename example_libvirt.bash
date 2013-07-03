6e5e742a23b786f9a56cc70d62741ebd641826ce
Dan Prince <dprince@redhat.com>
2013-07-03 13:18:33 -0400

    Update examples to use build:packages.
    Replace the individual package build tasks with the new consolidated
    build:packages task which uses config files for its configuration.

--- example_libvirt.bash
+++ example_libvirt.bash
@@ -3,49 +3,7 @@ export DISTRO_NAME=fedora
 rake kytoon:create GROUP_CONFIG="config/server_group_libvirt.json"
 
 rake build_misc
-
-rake nova:build_packages \
-	SOURCE_URL="git://github.com/openstack/nova.git" \
-	SOURCE_BRANCH="master"
-
-rake glance:build_packages \
-	SOURCE_URL="git://github.com/openstack/glance.git" \
-	SOURCE_BRANCH="master"
-
-rake swift:build_packages \
-	SOURCE_URL="git://github.com/openstack/swift.git" \
-	SOURCE_BRANCH="master"
-
-rake keystone:build_packages \
-        SOURCE_URL="git://github.com/openstack/keystone.git" \
-	SOURCE_BRANCH="master"
-
-rake cinder:build_packages \
-	SOURCE_URL="git://github.com/openstack/cinder.git" \
-	SOURCE_BRANCH="master"
-
-rake quantum:build_packages \
-	SOURCE_URL="git://github.com/openstack/quantum.git" \
-	SOURCE_BRANCH="master"
-
-rake nova:build_python_novaclient \
-	SOURCE_URL="git://github.com/openstack/python-novaclient.git"
-
-rake glance:build_python_glanceclient \
-	SOURCE_URL="git://github.com/openstack/python-glanceclient.git"
-
-rake keystone:build_python_keystoneclient \
-	SOURCE_URL="git://github.com/openstack/python-keystoneclient.git"
-
-rake cinder:build_python_cinderclient \
-	SOURCE_URL="git://github.com/openstack/python-cinderclient.git"
-
-rake swift:build_python_swiftclient \
-	SOURCE_URL="git://github.com/openstack/python-swiftclient.git"
-
-rake quantum:build_python_quantumclient \
-	SOURCE_URL="git://github.com/openstack/python-quantumclient.git"
-
+rake build:packages # see config/packages
 
 # required for Torpedo
 rake fog:build_packages
--- example_xen.bash
+++ example_xen.bash
@@ -29,48 +29,7 @@ rake kytoon:create \
 export SERVER_NAME="login"
 
 rake build_misc
-
-rake nova:build_packages \
-    SOURCE_URL="git://github.com/openstack/nova.git" \
-    SOURCE_BRANCH="master" GIT_MERGE="master"
-
-rake glance:build_packages \
-    SOURCE_URL="git://github.com/openstack/glance.git" \
-    SOURCE_BRANCH="master" GIT_MERGE="master"
-
-rake swift:build_packages \
-    SOURCE_URL="git://github.com/openstack/swift.git" \
-    SOURCE_BRANCH="master" GIT_MERGE="master"
-
-rake keystone:build_packages \
-    SOURCE_URL="git://github.com/openstack/keystone.git" \
-    SOURCE_BRANCH="master" GIT_MERGE="master"
-
-rake cinder:build_packages \
-    SOURCE_URL="git://github.com/openstack/cinder.git" \
-    SOURCE_BRANCH="master" GIT_MERGE="master"
-
-rake quantum:build_packages \
-    SOURCE_URL="git://github.com/openstack/quantum.git" \
-    SOURCE_BRANCH="master" GIT_MERGE="master"
-
-rake nova:build_python_novaclient \
-    SOURCE_URL="git://github.com/openstack/python-novaclient.git"
-
-rake glance:build_python_glanceclient \
-    SOURCE_URL="git://github.com/openstack/python-glanceclient.git"
-
-rake keystone:build_python_keystoneclient \
-    SOURCE_URL="git://github.com/openstack/python-keystoneclient.git"
-
-rake cinder:build_python_cinderclient \
-    SOURCE_URL="git://github.com/openstack/python-cinderclient.git"
-
-rake swift:build_python_swiftclient \
-    SOURCE_URL="git://github.com/openstack/python-swiftclient.git"
-
-rake quantum:build_python_quantumclient \
-    SOURCE_URL="git://github.com/openstack/python-quantumclient.git"
+rake build:packages # see config/packages
 
 rake create_package_repo
 
